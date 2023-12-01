16f53ffaf63740b4ffe39d70df1a6bd1f501291c
Franklin Tavares <franklinjunior12@hotmail.com>
2023-12-01 11:49:45 -0300

    c++20 and add version

---
+++ include/ArcaneVersion.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+
+#define MAJOR 0
+#define MINOR 1
+#define PATCH 0
+#define BUILD 0
+
+std::string arcane_version_string() {
+	char b[128];
+	sprintf(b, "%d.%d.%d.%d", MAJOR, MINOR, PATCH, BUILD);
+	return std::string(b);
+}
