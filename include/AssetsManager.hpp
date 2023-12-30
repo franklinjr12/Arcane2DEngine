ea89e2636197537c8e8d80e60c369c9ce3f009f4
Franklin Tavares <franklinjunior12@hotmail.com>
2023-12-30 16:10:35 -0300

    assets manager

--- include/AssetsManager.hpp
+++ include/AssetsManager.hpp
@@ -2,14 +2,20 @@
 
 #include "Arcane2DLib.hpp"
 #include "FontsManager.hpp"
+#include "Image.hpp"
+
+#include <unordered_map>
 
 class A2D_API AssetsManager {
 public:
 
 	static AssetsManager& get_instance();
-	static Font* get_default_font();
+	Font* get_default_font();
+	Image* get_image(std::string asset_name);
 
 private:
 	//static AssetsManager am;
 	AssetsManager() {}
+
+	std::unordered_map<std::string, Image*> images_map;
 };
--- src/AssetsManager.cpp
+++ src/AssetsManager.cpp
@@ -10,3 +10,14 @@ AssetsManager& AssetsManager::get_instance() {
 Font* AssetsManager::get_default_font() {
 	return FontsManager::get_instance()->default_font;
 }
+
+Image* AssetsManager::get_image(std::string asset_name) {
+	if (images_map.at(asset_name))
+		return images_map.at(asset_name);
+	Image* img = new Image("assets/" + asset_name);
+	if (img) {
+		images_map[asset_name] = img;
+		return img;
+	}
+	return nullptr;
+}
